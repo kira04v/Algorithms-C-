443dd3a81b9e066dfb770a60a1c9ab19f3ac46e3
Ayush Srivastava <80287117+kira04v@users.noreply.github.com>
2021-05-28 04:58:26 +0530

    Update 14. Prime Check.cpp

--- 14. Prime Check.cpp
+++ 14. Prime Check.cpp
@@ -4,18 +4,25 @@ int main()
 {
     int n,i,j=1;
     cin>>n;
-    for(i=2;i<=(n/2);i++)
-        {
-            if(n%i==0)
-                {
-                    cout<<"Not prime";
-                    j=0;
-                    break;
-                }
-        }
-    if(j==1)
-        {
-            cout<<"prime";
-        }
+    if(n==2)
+    {
+    cout<<"prime";
+    }
+    else
+    {
+        for(i=2;i<=(n/2);i++)
+            {
+                if(n%i==0)
+                    {
+                        cout<<"Not prime";
+                        j=0;
+                        break;
+                    }
+            }
+        if(j==1)
+            {
+                cout<<"prime";
+            }
+    }
     return 0;
 }
