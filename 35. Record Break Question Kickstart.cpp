74547e5e9cdd755b76554e54ec1931018b501b5f
Ayush Srivastava <80287117+kira04v@users.noreply.github.com>
2021-11-17 16:17:54 +0530

    Add files via upload

---
+++ 35. Record Break Question Kickstart.cpp
@@ -0,0 +1,72 @@
+/*
+The task is to print no. of record breaking days, as an array is taking for no. of customers on n no. of days
+conditions are for record break is that, 
+    1. No. of customers on the day of record break should be more than that of all the preceeding days
+    2. No. of customers on the day of record break should be more than the next day
+*/
+#include <iostream>
+#include <climits>
+using namespace std;
+void recbr(int n ,int arr[])
+{
+    int ans=0,temp=0;
+    for(int i=0;i<n;i++)
+    {
+        if(i==0)
+        {
+            if(arr[i]>arr[i+1])
+                    {
+                        ans++;
+                    }
+        }
+        else if(i>0 && i<n-1)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(arr[j]<arr[i])
+                {
+                    temp++;
+                }
+            }
+            if(temp==i)
+            {
+                if(arr[i]>arr[i+1])
+                    {
+                        ans++;
+                    }
+            }
+        temp=0;
+        }
+        else if(i==n-1)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(arr[j]<arr[i])
+                {
+                    temp++;
+                }
+            }
+            if(temp==i)
+            {
+                if(arr[i]>arr[i+1])
+                    {
+                        ans++;
+                    }
+            }
+        }
+    }
+    cout<<ans;
+    return;
+}
+int main()
+{
+    int a;
+    cin>>a;
+    int arr[a];
+    for(int i=0;i<a;i++)
+        {
+            cin>>arr[i];
+        }
+    recbr(a,arr);
+    return 0;
+}
